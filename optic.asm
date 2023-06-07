0410335f0c2f803a011c01ba9af65db20fd2ea96
Gavriel Linoy <71229809+Gavriel770U@users.noreply.github.com>
2023-06-07 11:29:08 +0300

    Update optic.asm
    Add start and instructions screens

--- optic.asm
+++ optic.asm
@@ -3,6 +3,18 @@ MODEL SMALL
 STACK 100h 
 DATASEG 
 
+; ---------------------        bmp files data        --------------------
+
+	menu_file_name db 'omenu.bmp',0
+	instructions_file_name db 'oinst.bmp', 0
+	filehandle dw ?
+	Header db 54 dup (0)
+	Palette db 256*4 dup (0)
+	ScrLine db 320 dup (0)
+	ErrorMsg db 'Error', 13, 10 ,'$'
+
+;------------------------------------------------------------------------
+
 ; ---------------------    color pick buffer data    ---------------------
 
   colorPickBuffer db 27, 27, 27, 27, 27, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 27, 27, 27, 27, 27, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
@@ -726,6 +738,136 @@ proc clearGraphicScreen
 	pop bp 
 	ret 2 
 endp clearGraphicScreen
+
+;=====================================================================================
+;===========================     BMP  FILES  FUNCTIONS     ===========================
+;=====================================================================================
+
+proc OpenMenuFile
+	; Open file
+	mov ah, 3Dh
+	xor al, al
+	mov dx, offset menu_file_name
+	int 21h
+	jc openerror
+	mov [filehandle], ax
+	ret
+	openerror :
+	mov dx, offset ErrorMsg
+	mov ah, 9h
+	int 21h
+	ret
+endp OpenMenuFile
+
+proc OpenInstFile
+	; Open file
+	mov ah, 3Dh
+	xor al, al
+	mov dx, offset instructions_file_name
+	int 21h
+	jc openerror2
+	mov [filehandle], ax
+	ret
+	openerror2:
+	mov dx, offset ErrorMsg
+	mov ah, 9h
+	int 21h
+	ret
+endp OpenInstFile
+
+proc ReadHeader
+	; Read BMP file header, 54 bytes
+	mov ah,3fh
+	mov bx, [filehandle]
+	mov cx,54
+	mov dx,offset Header
+	int 21h
+	ret
+endp ReadHeader
+
+proc ReadPalette
+	; Read BMP file color palette, 256 colors * 4 bytes (400h)
+	mov ah,3fh
+	mov cx,400h
+	mov dx,offset Palette
+	int 21h
+	ret
+endp ReadPalette
+
+proc CopyPal
+	; Copy the colors palette to the video memory
+	; The number of the first color should be sent to port 3C8h
+	; The palette is sent to port 3C9h
+	mov si,offset Palette
+	mov cx,256
+	mov dx,3C8h
+	mov al,0
+	; Copy starting color to port 3C8h
+	out dx,al
+	; Copy palette itself to port 3C9h
+	inc dx
+	PalLoop:
+	; Note: Colors in a BMP file are saved as BGR values rather than RGB .
+	mov al,[si+2] ; Get red value .
+	shr al,2 ; Max. is 255, but video palette maximal
+	; value is 63. Therefore dividing by 4.
+	out dx,al ; Send it .
+	mov al,[si+1] ; Get green value .
+	shr al,2
+	out dx,al ; Send it .
+	mov al,[si] ; Get blue value .
+	shr al,2
+	out dx,al ; Send it .
+	add si,4 ; Point to next color .
+	; (There is a null chr. after every color.)
+	loop PalLoop
+	ret
+endp CopyPal
+
+proc CopyBitmap
+	; BMP graphics are saved upside-down .
+	; Read the graphic line by line (200 lines in VGA format),
+	; displaying the lines from bottom to top.
+	mov ax, 0A000h
+	mov es, ax
+	mov cx,200
+PrintBMPLoop:
+	push cx
+	; di = cx*320, point to the correct screen line
+	mov di,cx
+	shl cx,6
+	shl di,8
+	add di,cx
+	; Read one line
+	mov ah,3fh
+	mov cx,320
+	mov dx,offset ScrLine
+	int 21h
+	; Copy one line into video memory
+	cld ; Clear direction flag, for movsb
+	mov cx,320
+	mov si,offset ScrLine
+	rep movsb ; Copy line to the screen
+	;rep movsb is same as the following code :
+	;mov es:di, ds:si
+	;inc si
+	;inc di
+	;dec cx
+	;loop until cx=0
+	pop cx
+	loop PrintBMPLoop
+	ret
+endp CopyBitmap
+
+proc CloseFile
+	mov ah,3Eh
+	mov bx, [filehandle]
+	int 21h
+	ret
+endp CloseFile
+
+;=====================================================================================
+;=====================================================================================
 
 proc pointCoordinatesToPixelCoordinate
 	; this procedure gets an (x,y) coordinate and converts it to coordinate on the graphic screen
@@ -2428,6 +2570,82 @@ start:
 	
 	mov ax, 1h ; put mouse 
 	int 33h 
+	
+	xor ax, ax 
+	xor bx, bx 
+	xor cx, cx 
+	xor dx, dx 
+	xor si, si 
+	xor di, di
+	mov es, ax
+	
+	call OpenMenuFile
+	call ReadHeader
+	call ReadPalette
+	call CopyPal
+	call CopyBitmap
+	
+noMenuCloseInput:
+	xor al, al
+	mov ah, 1h 
+	int 16h
+	jz noMenuCloseInput 
+	xor al, al
+	mov ah, 0h
+	int 16h
+	
+	call closeFile
+	push GRAPHICAL_SCREEN_VALUE
+	call clearGraphicScreen
+	
+	; back to text mode
+	mov ah, 0
+	mov al, 2
+	int 10h
+	; back to graphics mode 
+	mov ax, 13h 
+	int 10h
+
+	xor ax, ax 
+	xor bx, bx 
+	xor cx, cx 
+	xor dx, dx 
+	xor si, si 
+	xor di, di
+	
+	call OpenInstFile
+	call ReadHeader
+	call ReadPalette
+	call CopyPal
+	call CopyBitmap
+	
+noInstCloseInput:
+	xor al, al
+	mov ah, 1h 
+	int 16h
+	jz noInstCloseInput 
+	xor al, al
+	mov ah, 0h
+	int 16h
+	
+	call closeFile
+	push GRAPHICAL_SCREEN_VALUE
+	call clearGraphicScreen
+	
+	; back to text mode
+	mov ah, 0
+	mov al, 2
+	int 10h
+	; back to graphics mode 
+	mov ax, 13h 
+	int 10h
+
+	xor ax, ax 
+	xor bx, bx 
+	xor cx, cx 
+	xor dx, dx 
+	xor si, si 
+	xor di, di 
 
 	; push offset trianglesAmount        ; + 24
 	; push offset trianglesXCoordinates  ; + 22
@@ -2682,6 +2900,5 @@ END start
 
 ; TODO
 ; 	**IMP. !!! ADD DOCUMENTATION FOR ALL FUNCTIONS !!! 
-;	    1. add start screen to the code.
 ;     2. add music
 ;			3. add save buffer to hide color pick buffer 
